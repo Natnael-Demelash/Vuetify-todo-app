aad9470b0ed0b32b11f1282081a2145667104abd
Natnael <natidemelash074@gmail.com>
2022-01-19 22:11:48 +0300

    finished vuetify todo app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit ipsam
+      facilis velit minus recusandae distinctio aliquid odit id eum! Illum, quo?
+      Accusantium inventore pariatur dolor perferendis ipsam enim obcaecati.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit ipsam
+      facilis velit minus recusandae distinctio aliquid odit id eum! Illum, quo?
+      Accusantium inventore pariatur dolor perferendis ipsam enim obcaecati.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit ipsam
+      facilis velit minus recusandae distinctio aliquid odit id eum! Illum, quo?
+      Accusantium inventore pariatur dolor perferendis ipsam enim obcaecati.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum sit ipsam
+      facilis velit minus recusandae distinctio aliquid odit id eum! Illum, quo?
+      Accusantium inventore pariatur dolor perferendis ipsam enim obcaecati.
+    </p>
   </div>
 </template>
+
+<style scoped>
+p {
+  font-size: 18px;
+  width: 90%;
+  font-weight: 700;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,132 @@
 <template>
-  <div>
-    <v-container>
-      <h2 class="ma-10 display-3">Home</h2>
-    <p class="pink lighten-2 white--text pa-15 text-center ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-      Earum molestias nulla aspernatur soluta! Modi unde alias autem cum adipisci laboriosam numquam? 
-      Illum placeat, temporibus soluta molestiae accusantium nesciunt sed cumque?</p>
+  <div class="home pa-6">
+    <v-text-field
+      class="pa-3"
+      v-model="newTaskTitle"
+      @click:append="addTask"
+      @keyup.enter="addTask"
+      clearable
+      label="Add Task"
+      append-icon="mdi-plus"
+    ></v-text-field>
+    <p class="err pl-6" v-if="errMsg">Please Add a Task</p>
+    <div class="flex">
+      <v-list flat subheader three-line>
+        <v-subheader>General</v-subheader>
 
-      <v-btn  class="purple  lighten-2 mx-15 py-7 px-10 white--text">Login</v-btn>
+        <div v-for="task in tasks" :key="task.id">
+          <v-list-item
+            @click="doneTask(task.id)"
+            :class="{ 'red lighten-4': task.done }"
+          >
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox :input-value="task.done"></v-checkbox>
+              </v-list-item-action>
 
-      <v-btn depressed class="amber py-7 px-10 lighten-4 black--text">
-        <v-icon left>people</v-icon>
-        <span>Contact</span>
-      </v-btn>
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{ 'text-decoration-line-through': task.done }"
+                  class="main-title"
+                  >{{ task.title }}</v-list-item-title
+                >
+                <v-list-item-subtitle class="subtitle">{{
+                  task.desc
+                }}</v-list-item-subtitle>
+              </v-list-item-content>
 
-    </v-container>
-    
+              <v-list-item-action>
+                <v-btn icon @click.stop="deleTask(task.id)">
+                  <v-icon color="black lighten-1">mdi-delete-empty</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+
+          <v-divider></v-divider>
+        </div>
+      </v-list>
+
+      <v-spacer></v-spacer>
+
+      <h1 class="right mr-10">Today's Task!</h1>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      newTaskTitle: "",
+      errMsg: false,
+      tasks: [
+        {
+          id: 1,
+          title: "Task 1",
+          desc: "Notify me about updates to apps or games that I downloaded",
+          done: false,
+        },
+        {
+          id: 2,
+          title: "Task 2",
+          desc: "Notify me about updates to apps or games that I downloaded",
+          done: false,
+        },
+        {
+          id: 3,
+          title: "Task 3",
+          desc: "Notify me about updates to apps or games that I downloaded",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTaskTitle.trim() !== "") {
+        let newTask = {
+          id: Date.now(),
+          title: this.newTaskTitle,
+          done: false,
+        };
+        this.tasks.push(newTask);
+        this.newTaskTitle = "";
+      } else {
+        this.errMsg = true;
+      }
+    },
+    doneTask(id) {
+      let task = this.tasks.filter((task) => task.id === id)[0];
+      task.done = !task.done;
+    },
+    deleTask(id) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.right {
+  font-size: 80px;
+  color: rgba(95, 16, 95, 0.808);
+}
+.main-title {
+  font-size: 22px;
+}
+.subtitle {
+  font-size: 18px;
+}
 
+.err {
+  color: red;
+  font-size: 22px;
+  font-style: italic;
+}
 </style>
